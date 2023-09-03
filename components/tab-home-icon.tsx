7608fbca79b7685f27107cf13e0a3f93b53c0a1d
wxu <ginues109@gmail.com>
2023-09-03 11:07:27 -0700

    tab icons

---
+++ components/tab-home-icon.tsx
@@ -0,0 +1,7 @@
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons'; 
+import React from "react";
+
+export const TabHomeIcon = () => <Ionicons name="home-outline" size={24} color="black" />;
+export const TabDiscoverIcon = () => <Ionicons name="compass-outline" size={24} color="black" />;
+export const TabProfileIcon = () => <Ionicons name="ios-settings-outline" size={24} color="black" />
